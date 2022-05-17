16f7ee2c4775499ec8b1480df0a52f5b46875086
Santosh Poudel <99386346+Santo9851@users.noreply.github.com>
2022-05-17 11:04:23 +0545

    Built Cache for data

--- election_data.py
+++ election_data.py
@@ -6,7 +6,7 @@ province_no = st.selectbox('Please enter province number', options = ['1','2','3
 
 province_url = 'https://result.election.gov.np/JSONFiles/Election2079/Local/VoteCountProvince'+province_no+'.json'
 
-
+@st.cache
 def scrapper(url):
     province = pd.read_json(url)
     province = pd.DataFrame(province)
